6b8b263f21edb94a8882e7ca2a7c5d98d3c2d977
Shantanu <shantanupardeshi4@gmail.com>
2024-04-01 17:13:52 +0530

    Done with login signup , home page frontend and navigation part

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import Navbar from "../features/navbar/Navbar";
+import ProductList from "../features/products_list/ProductList";
+
+function Home()
+{
+    return(
+        <div>
+            <Navbar>
+                <ProductList></ProductList>
+            </Navbar>
+        </div>
+
+    );
+}
+
+export default Home;
