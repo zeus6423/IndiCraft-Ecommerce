51f4f45c05ab14140a734aa2740e6f4149695b3c
Shantanu <shantanupardeshi4@gmail.com>
2024-04-13 17:32:02 +0530

    Up to date code , cart as well

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
 } from "react-router-dom";
-import Login from './features/auth/components/Login';
-import Cart from './features/cart/Cart';
-import CartPage from './pages/CartPage';
-import Checkout from './pages/Checkout';
-import ProductDetail from './features/product/components/ProductDetail';
-import ProductDetailPage from './pages/ProductDetailPage';
-import Protected from './features/auth/components/protected';
+import Login from "./features/auth/components/Login";
+import Cart from "./features/cart/Cart";
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
+import ProductDetail from "./features/product/components/ProductDetail";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import Protected from "./features/auth/components/protected";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchItemsByUserIdAsync } from "./features/cart/CartSlice";
+import { selectLoggedInUser } from "./features/auth/authSlice";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Protected><Home></Home></Protected>
-    
+    element: (
+      <Protected>
+        <Home></Home>
+      </Protected>
+    ),
   },
   {
     path: "/login",
@@ -29,26 +36,42 @@ const router = createBrowserRouter([
   },
   {
     path: "/signup",
-    element: <SignupPage></SignupPage>
+    element: <SignupPage></SignupPage>,
   },
   {
-   
     path: "/cart",
-    element: <Protected><CartPage></CartPage></Protected>
+    element: (
+      <Protected>
+        <CartPage></CartPage>
+      </Protected>
+    ),
   },
   {
-    
     path: "/checkout",
-    element: <Protected><Checkout></Checkout></Protected>
+    element: (
+      <Protected>
+        <Checkout></Checkout>
+      </Protected>
+    ),
   },
   {
-    
     path: "/product-detail/:id",
-    element: <Protected><ProductDetailPage></ProductDetailPage></Protected>
+    element: (
+      <Protected>
+        <ProductDetailPage></ProductDetailPage>
+      </Protected>
+    ),
   },
 ]);
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchItemsByUserIdAsync(user.id));
+    }
+  }, [dispatch,user]);
   return (
     <div className="App">
       <RouterProvider router={router} />
--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -1,23 +1,47 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './CartApi';
-
+import { addToCart , deleteItemFromCart, fetchItemsByUserId, updateItem} from './CartApi';
 const initialState = {
-  value: 0,
   status: 'idle',
+  items:[],
+
 };
 
 
-export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount) => {
-    const response = await fetchCount(amount);
+export const addToCartAsync = createAsyncThunk(
+  'cart/addToCart',
+  async (item) => {
+    const response = await addToCart(item);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+export const fetchItemsByUserIdAsync = createAsyncThunk(
+  'cart/fetchItemsByUserId',
+  async (userId) => {
+    const response = await fetchItemsByUserId(userId);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+export const updateItemAsync = createAsyncThunk(
+  'cart/updateItem',
+  async (update) => {
+    const response = await updateItem(update);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+export const deleteItemFromCartAsync = createAsyncThunk(
+  'cart/deleteItemFromCart',
+  async (itemId) => {
+    const response = await deleteItemFromCart(itemId);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const cartSlice = createSlice({
+  name: 'cart',
   initialState,
   
   reducers: {
@@ -28,21 +52,41 @@ export const counterSlice = createSlice({
  
   extraReducers: (builder) => {
     builder
-      .addCase(incrementAsync.pending, (state) => {
+      .addCase(addToCartAsync.pending, (state) => {
         state.status = 'loading';
       })
-      .addCase(incrementAsync.fulfilled, (state, action) => {
+      .addCase(addToCartAsync.fulfilled, (state, action) => {
         state.status = 'idle';
-        state.value += action.payload;
-      });
+        state.items.push(action.payload);
+      })
+      .addCase(fetchItemsByUserIdAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchItemsByUserIdAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.items=action.payload;
+      })
+      .addCase(updateItemAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(updateItemAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        const index= state.items.findIndex(item=>item.id===action.payload.id)
+        state.items[index]=action.payload;
+      })
+      .addCase(deleteItemFromCartAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(deleteItemFromCartAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        const index= state.items.findIndex(item=>item.id===action.payload.id)
+        state.items.splice(index,1)
+      }
+    )
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const selectItems = (state) => state.cart.items
 
 
-export const selectCount = (state) => state.counter.value;
-
-
-
-export default counterSlice.reducer;
+export default cartSlice.reducer;
